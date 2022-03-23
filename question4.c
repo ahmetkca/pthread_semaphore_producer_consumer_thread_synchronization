fbe1805ea54e32026f84047d9240ff784f29abfa
ahmetkca <ahmet.karapinar@ontariotechu.net>
2022-03-23 00:02:13 -0400

    producer/consumer shared buffer race condition

--- question4.c
+++ question4.c
@@ -12,87 +12,118 @@
 #define MAX_BUFF_ARRAY_LEN 5
 #define NUMBERS_LEN 10
 
-static int *buff_array;
-static int curr_size = 0;
+static sem_t sem;
 
-
+int buff_array[MAX_BUFF_ARRAY_LEN] = { 0, 0, 0, 0, 0 };
+int curr_buff_index = 0;
 
 typedef struct {
     int *nums;
 } producer_args_t;
 
-void add_to_buffer(int num);
-int remove_last_from_buffer();
-
+void display_buffer();
 void *producer(void *vptr);
 void *consumer(void *vptr);
 
 
 int main(void) 
 {
-    add_to_buffer(10);
-    add_to_buffer(20);
-    printf("%d\n", buff_array[0]);
-    printf("%d\n", buff_array[1]);
-    int rem = remove_last_from_buffer();
-    printf("%d\n", rem);
-    return 0;
+    sem_init(&sem, 0, 1);
     srand(time(NULL));
     int numbers[NUMBERS_LEN] = { 0, 0, 0, 0, 0,
                                     0, 0, 0, 0, 0};
     pthread_t pro_tid, con_tid;
+
+    /* prompt user for 10 numbers */
     for (int i = 0;i < NUMBERS_LEN; i++) {
         printf("Enter a number for numbers[%d]: ", i);
         scanf("%d", &numbers[i]);
     }
+
+    /* create thread for both producer and consumer */
     producer_args_t pro_args = { .nums=&numbers[0] };
     pthread_create(&pro_tid, NULL, &producer, (void *) &pro_args);
+    pthread_create(&con_tid, NULL, &consumer, NULL);
     pthread_join(pro_tid, NULL);
+    pthread_join(con_tid, NULL);
+
+    printf("Content of buffer array after both producer and consumer threads\n");
+    for (int i = 0; i < MAX_BUFF_ARRAY_LEN; i++)
+    {
+        printf("buff_array[%d] = %d\n", i, buff_array[i]);
+    }
+
+    sem_destroy(&sem);
     return 0;
 }
 
-void add_to_buffer(int num)
-{
-    if (buff_array == NULL && curr_size == 0) {
-        buff_array = (int *) malloc(sizeof(int));
-        buff_array[0] = num;
-        curr_size++;
-    }
-    else if (curr_size <= MAX_BUFF_ARRAY_LEN) {
-        buff_array = (int *) realloc(buff_array, (curr_size + 1) * sizeof(int));
-        buff_array[curr_size] = num;
-        curr_size++;
-    }
-}
 
-int remove_last_from_buffer()
-{
-    if (curr_size > 0) {
-        int n = buff_array[curr_size-1];
-        free(buff_array[curr_size-1]);
-        curr_size--;
-        return n;
-    }
-    return -1;
-}
-
+/* takes array of numbers 
+ * add each number to buffer 
+ */
 void *producer(void *vptr)
 {
     producer_args_t *producer_args = (producer_args_t *) vptr;
-
-    for (int i = 0; i < NUMBERS_LEN; i++)
-    {
-        sleep(rand() % 10 + 1);
-
+    int produced = 0;
+    while (produced < NUMBERS_LEN) {
+        /* sleep for random amount of time */
+        int sleep_time = rand() % 10 + 1;
+        printf("Producer sleeping for %d seconds\n", sleep_time);
+        sleep(sleep_time);
+        /* critical section starts */
+        // add number from numbers array to buffer
+        if (curr_buff_index < MAX_BUFF_ARRAY_LEN)
+        {
+            sem_wait(&sem);
+            // number from numbers array to buffer
+            buff_array[curr_buff_index] = producer_args->nums[produced];
+            printf("Produced %d\n", buff_array[curr_buff_index]);
+            curr_buff_index++;
+            produced++;
+            display_buffer();
+            sem_post(&sem);
+        } else {
+            /* buffer is full wait for consumer*/
+            printf("Buffer is full\n");
+            while (curr_buff_index == (MAX_BUFF_ARRAY_LEN - 1));
+        }
     }
     pthread_exit(NULL);
 }
 
+void display_buffer() {
+    printf("current buffer index is %d\n", curr_buff_index);
+    for (int i = 0; i < MAX_BUFF_ARRAY_LEN; i++) {
+        printf("%d ", buff_array[i]);
+    }
+    printf("\n");
+}
+
+/* consumes number from buffer produced by producer */
 void *consumer(void *vptr)
 {
-    for (int i = 0; i < NUMBERS_LEN; i++)
-    {
-        sleep(rand() % 10 + 1);
+    int consumed = 0;
+    while (consumed < NUMBERS_LEN) {
+        int sleep_time = rand() % 30 + 1;
+        printf("Consumer sleeping for %d seconds\n", sleep_time);
+        sleep(sleep_time);
+        /* consume added numbers from buffer */
+        if (curr_buff_index != -1 &&  curr_buff_index >= 0)
+        {
+            sem_wait(&sem);
+            /* critical section starts */
+            /* buffer has number in it, consume it */
+            printf("Consumed %d\n", buff_array[curr_buff_index-1]);
+            buff_array[curr_buff_index-1] = 0;
+            curr_buff_index--;
+            consumed++;
+            display_buffer();
+            sem_post(&sem);
+        } else {
+            /* buffer is empty wait for producer */
+            printf("Buffer is empty\n");
+            while (curr_buff_index == 0);
+        }
 
     }
     pthread_exit(NULL);
